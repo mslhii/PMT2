a0267faa6e7bc41000c614cf2ad72504ea0a1406
Michael Hii <mslhii@yahoo.com>
2016-11-04 02:42:08 -0700

    Taking out deprecated code

--- app/src/main/java/com/kritikalerror/pmt2/SplashActivity.java
+++ app/src/main/java/com/kritikalerror/pmt2/SplashActivity.java
@@ -92,48 +92,7 @@ public class SplashActivity extends ActionBarActivity {
         return true;
     }
 
-//    private boolean initializeWrapperDeprecated() {
-//        int hasCameraPermission = ContextCompat.checkSelfPermission(SplashActivity.this,
-//                Manifest.permission.SEND_SMS);
-//        if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
-//            if (!ActivityCompat.shouldShowRequestPermissionRationale(SplashActivity.this,
-//                    Manifest.permission.SEND_SMS)) {
-//                showOKAlertMessage("You need to allow app to send SMS for the app to function properly",
-//                        new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                ActivityCompat.requestPermissions(SplashActivity.this,
-//                                        new String[]{Manifest.permission.SEND_SMS},
-//                                        REQUEST_CODE_ASK_SMS_PERMISSIONS);
-//                            }
-//                        });
-//            }
-//            ActivityCompat.requestPermissions(SplashActivity.this,
-//                    new String[] {Manifest.permission.SEND_SMS},
-//                    REQUEST_CODE_ASK_SMS_PERMISSIONS);
-//        }
-//
-//        int hasWriteStoragePermission = ContextCompat.checkSelfPermission(SplashActivity.this,
-//                Manifest.permission.READ_CONTACTS);
-//        if (hasWriteStoragePermission != PackageManager.PERMISSION_GRANTED) {
-//            if (!ActivityCompat.shouldShowRequestPermissionRationale(SplashActivity.this,
-//                    Manifest.permission.READ_CONTACTS)) {
-//                showOKAlertMessage("You need to allow access to contacts for the app to function properly",
-//                        new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                ActivityCompat.requestPermissions(SplashActivity.this,
-//                                        new String[]{Manifest.permission.READ_CONTACTS},
-//                                        REQUEST_CODE_ASK_CONTACT_PERMISSIONS);
-//                            }
-//                        });
-//            }
-//            ActivityCompat.requestPermissions(SplashActivity.this,
-//                    new String[] {Manifest.permission.READ_CONTACTS},
-//                    REQUEST_CODE_ASK_CONTACT_PERMISSIONS);
-//        }
-//        return true;
-//    }
+
 
     private void showOKAlertMessage(String message, DialogInterface.OnClickListener okListener) {
         new android.support.v7.app.AlertDialog.Builder(SplashActivity.this)
